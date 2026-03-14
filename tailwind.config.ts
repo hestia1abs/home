e51135552b18543ef09d0ac62f0dbb66b98d36cb
dedsec <dedsec@leviathan.0xdedsec>
2026-03-14 23:46:14 +0530

    refactor: Update Tailwind CSS content paths to include the 'src' directory.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import animate from "tailwindcss-animate";
 
 const config: Config = {
     darkMode: "class",
-    content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./lib/**/*.{ts,tsx}"],
+    content: ["./src/app/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}", "./src/lib/**/*.{ts,tsx}"],
     theme: {
         extend: {
             colors: {
